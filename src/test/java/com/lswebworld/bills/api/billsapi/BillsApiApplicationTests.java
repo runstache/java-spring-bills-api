ba584076404c9ea71db523dc944380ae1d5223f1
Larry Shields <larry.shields@gmail.com>
2022-07-09 11:17:38 -0400

    Initial Project creation

---
+++ src/test/java/com/lswebworld/bills/api/billsapi/BillsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lswebworld.bills.api.billsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
